74d2cdb638fdb613e6af7a77e9ca52be1549e2f3
Richard Trey  Hoover <rkhoover@macmini.visionist.local>
2016-07-26 15:49:24 -0400

    Use padding

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function GenerateJsonPlugin(filename, obj) {
 
 GenerateJsonPlugin.prototype.apply = function apply(compiler) {
   compiler.plugin('emit', (compilation, done) => {
-    const json = JSON.stringify(this.obj);
+    const json = JSON.stringify(this.obj, null, 2);
     compilation.assets[this.filename] = { // eslint-disable-line no-param-reassign
       source: () => json,
       size: () => json.length,
