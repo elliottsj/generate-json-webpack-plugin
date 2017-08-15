25937fd1fbc097ea86e4b1a496311db1cf53da1b
Ng Yik Phang <ngyikp@gmail.com>
2017-08-16 01:34:35 +0800

    Improve performance for Webpack watch mode
    Previously, the file would be re-generated on every change/emit of
    other files, which is unnecessary

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 function GenerateJsonPlugin(filename, value, replacer, space) {
   Object.assign(this, { filename, value, replacer, space });
+
+  this.previousJson = null;
 }
 
 GenerateJsonPlugin.prototype.apply = function apply(compiler) {
   compiler.plugin('emit', (compilation, done) => {
     const json = JSON.stringify(this.value, this.replacer, this.space);
+
+    // For faster performance on watch mode
+    if (this.previousJson === json) {
+      done();
+      return;
+    }
+
     compilation.assets[this.filename] = {
       source: () => json,
       size: () => json.length,
     };
+
+    this.previousJson = json;
     done();
   });
 };
