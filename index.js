e55cd3d68350f2782f3a72e2f8cb92fe815f2157
Spencer Elliott <me@elliottsj.com>
2020-05-02 14:20:06 -0400

    Revert `previousJson` optimization
    Fixes #66

--- index.js
+++ index.js
@@ -2,24 +2,16 @@ function GenerateJsonPlugin(filename, value, replacer, space) {
   Object.assign(this, { filename, value, replacer, space });
 
   this.plugin = { name: 'GenerateJsonPlugin' };
-
-  this.previousJson = null;
 }
 
 GenerateJsonPlugin.prototype.apply = function apply(compiler) {
   const emit = (compilation, callback) => {
     const json = JSON.stringify(this.value, this.replacer, this.space);
 
-    if (this.previousJson === json) {
-      return callback(null);
-    }
-
     compilation.assets[this.filename] = {
       source: () => json,
       size: () => json.length,
     };
-
-    this.previousJson = json;
 
     callback(null);
   };
