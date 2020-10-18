6ad4fd7dcf024d766b712e180b4831cb583c547f
Spencer Elliott <me@elliottsj.com>
2020-10-18 15:44:35 -0400

    Ensure example build dir is deleted before test

--- example/__tests__/example-test.js
+++ example/__tests__/example-test.js
@@ -1,11 +1,13 @@
+const del = require('del');
 const fs = require('fs');
 const path = require('path');
 const webpack = require('webpack');
 const config = require('../webpack.config');
 
 describe('example', () => {
-  it('creates a file my-file.json', () =>
-    new Promise((resolve, reject) => {
+  it('creates a file my-file.json', async () => {
+    await del(path.join(__dirname, '..', 'build'));
+    await new Promise((resolve, reject) => {
       webpack(config, (error, stats) => {
         if (error) {
           reject(error);
@@ -24,5 +26,6 @@ describe('example', () => {
           }
         );
       });
-    }));
+    });
+  });
 });
